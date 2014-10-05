aef97f876ffbe44fec56e1b188e8a5ec6a7065f8
Mateusz Derks <mateusz.derks@gmail.com>
2014-10-05 12:52:49 +0200

    Configured code quality tools

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jscs: {
+            options: {
+                config: ".jscsrc",
+                reporter: "checkstyle",
+                reporterOutput: "logs/checkstyle_jscs.xml"
+            },
+            test: {
+                files: {
+                    src: [
+                        'lib/**/*.js',
+                        'index.js'
+                    ]
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+            test: {
+                files: {
+                    src: [
+                        'lib/**/*.js',
+                        'index.js'
+                    ]
+                },
+                options: {
+                    reporter: "checkstyle",
+                    reporterOutput: "logs/checkstyle_js.xml"
+                }
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'checkstyle',
+                    captureFile: 'logs/mocha.xml' // Optionally capture the reporter output to a file
+                },
+                src: ['test/**/*.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks("grunt-jscs");
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('default', ['test']);
+
+    grunt.registerTask('test', ['jscs', 'jshint:test', 'mochaTest']);
+};
